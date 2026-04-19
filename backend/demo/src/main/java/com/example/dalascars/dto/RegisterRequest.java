6e3da2187e4599096161fa36fad2b4b40935dea5
Ousama Elz <ousama850@gmail.com>
2026-04-20 01:19:27 +0200

    dtos

---
+++ backend/demo/src/main/java/com/example/dalascars/dto/RegisterRequest.java
@@ -0,0 +1,29 @@
+package com.example.dalascars.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    private String phone;
+    private String city;
+}
